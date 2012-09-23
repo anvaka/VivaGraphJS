e6d2602d012b91fb869c5b70ff1d98a288b51281
anvaka <anvaka@gmail.com>
2012-09-22 21:23:39 -0700

    New tutorial: Showing how to display two edges connecting the same nodes.

--- dist/vivagraph.js
+++ dist/vivagraph.js
@@ -947,6 +947,23 @@ Viva.Graph.geom = function() {
         }  
     };
 };/**
+ * Very generic rectangle. 
+ */
+Viva.Graph.Rect = function(x1, y1, x2, y2) {
+    this.x1 = x1 || 0;
+    this.y1 = y1 || 0;
+    this.x2 = x2 || 0;
+    this.y2 = y2 || 0;
+};
+
+/**
+ * Very generic two-dimensional point.
+ */
+Viva.Graph.Point2d = function(x, y) {
+    this.x = x || 0;
+    this.y = y || 0;
+}
+/**
  * @fileOverview Contains definition of the core graph object.
  *
  * @author Andrei Kashcha (aka anvaka) / http://anvaka.blogspot.com
@@ -2837,7 +2854,7 @@ Viva.Graph.Layout.forceDirected = function(graph, userSettings) {
         
         initializationRequired = true,
         
-        graphRect = {x1: 0, y1 : 0, x2 : 0, y2 : 0},
+        graphRect = new Viva.Graph.Rect(),
         
         random = Viva.random('ted.com', 103, 114, 101, 97, 116),
         
@@ -3097,6 +3114,120 @@ Viva.Graph.Layout.forceDirected = function(graph, userSettings) {
                 return dragForce.options().coeff; 
             }
         }
+    };
+};/*global Viva*/
+
+Viva.Graph.Layout = Viva.Graph.Layout || {};
+
+/**
+ * Does not really perform any layouting algorithm but is compliant 
+ * with renderer interface. Allowing clients to provide specific positioning
+ * callback and get static layout of the graph
+ * 
+ * @param {Viva.Graph.graph} graph to layout
+ * @param {Object} userSettings
+ */
+Viva.Graph.Layout.constant = function(graph, userSettings) {
+    userSettings = userSettings || {};
+    
+    var seed = userSettings.seed || 'Deterministic randomness made me do this',
+        maxX = (typeof userSettings.maxX === 'number') ? userSettings.maxX : 1024,
+        maxY = (typeof userSettings.maxY === 'number') ? userSettings.maxY : 1024,  
+        rand = Viva.random(seed),
+        
+        graphRect = new Viva.Graph.Rect(),
+        
+        placeNodeCallback = function(node) {
+             return new Viva.Graph.Point2d(rand.next(maxX), rand.next(maxY)); 
+        },
+        
+        updateNodePositions = function() {
+            var x1 = Number.MAX_VALUE,
+                y1 = Number.MAX_VALUE,
+                x2 = Number.MIN_VALUE,
+                y2 = Number.MIN_VALUE;
+            if (graph.getNodesCount() === 0) { return ;}
+               
+            graph.forEachNode(function(node) {
+                if (!node.hasOwnProperty('position')) {
+                    node.position = placeNodeCallback(node);
+                }
+
+                if (node.position.x < x1) { x1 = node.position.x; }
+                if (node.position.x > x2) { x2 = node.position.x; }
+                if (node.position.y < y1) { y1 = node.position.y; }
+                if (node.position.y > y2) { y2 = node.position.y; }
+            });
+            
+            graphRect.x1 = x1;
+            graphRect.x2 = x2;
+            graphRect.y1 = y1;
+            graphRect.y2 = y2;
+        };
+    
+    return {
+        /**
+         * Attempts to layout graph within given number of iterations.
+         *
+         * @param {integer} [iterationsCount] number of algorithm's iterations.
+         *  The constant layout ignores this parameter.
+         */
+        run : function(iterationsCount) {
+            this.step();
+        },
+        
+        /**
+         * One step of layout algorithm.
+         */
+        step : function() {
+            updateNodePositions();
+            
+            return false; // no need to continue.
+        },
+        
+        /**
+         * Returns rectangle structure {x1, y1, x2, y2}, which represents
+         * current space occupied by graph.
+         */
+        getGraphRect : function() {
+            return graphRect;
+        }, 
+        
+        addNode : function(node) { /* nop */ },
+        
+        removeNode : function(node) { /* nop */ },
+        
+        addLink : function(link) { /* nop */ },
+        
+        removeLink : function(link) { /* nop */ },
+        
+        // Layout specific methods:
+        
+        /**
+         * Based on argument either update default node placement callback or
+         * attempts to place given node using current placement callback. 
+         * Setting new node callback triggers position update for all nodes.
+         *  
+         * @param {Object} newPlaceNodeCallbackOrNode - if it is a function then
+         * default node placement callback is replaced with new one. Node placement
+         * callback has a form of function(node) {}, and is expected to return an 
+         * object with x and y properties set to numbers. 
+         * 
+         * Otherwise if it's not a function the argument is treated as graph node 
+         * and current node placement callback will be used to place it.
+         */
+        placeNode : function(newPlaceNodeCallbackOrNode) {
+            if (typeof(newPlaceNodeCallbackOrNode) === 'function') {
+                placeNodeCallback = newPlaceNodeCallbackOrNode;
+                updateNodePositions();
+                return this;
+            }
+            
+            // it is not a request to update placeNodeCallback, trying to place 
+            // a node using current callback:
+            return placeNodeCallback(newPlaceNodeCallbackOrNode);
+        }
+    
     };
 };/**
  * @fileOverview Defines a graph renderer that uses CSS based drawings.
