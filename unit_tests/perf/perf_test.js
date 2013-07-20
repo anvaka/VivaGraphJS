6a04c5c5344b84baa377022ed65cacbd3203c913
anvaka <anvaka@gmail.com>
2013-07-20 14:12:41 -0700

    Latest v8 is awesome. 35% performance improvement.

--- unit_tests/perf/perf_test.js
+++ unit_tests/perf/perf_test.js
@@ -1,12 +1,21 @@
+// When node is compiled with the lates v8 version performance gain is 35% .. 40%
+//
+// Take a loook:
+// V8 version: 3.20.2 (node version v0.11.5-pre+)
+// Grid 100x100, Nodes: 10000, Edges: 19800, 100 layout iterations. Layout time: 9141ms
+//
+// V8 version: 3.14.5.9 (node version v0.10.10)
+// Grid 100x100, Nodes: 10000, Edges: 19800, 100 layout iterations. Layout time: 14771ms
+
 var Viva = require('../../dist/vivagraph');
 var gridSize = 100;
 var layoutIterations = 100;
 
 console.log('This is a very basic performance test of force directed layout');
-console.log('Constructing graph: Grid ' + gridSize + 'x' + gridSize);
+console.log('V8 version: ' + process.versions.v8);
 var generator = Viva.Graph.generator();
 var graph = generator.grid(gridSize, gridSize);
-console.log('Nodes: ' + graph.getNodesCount() + ', Edges: ' + graph.getLinksCount());
+console.log('Grid ' + gridSize + 'x' + gridSize + ', Nodes: ' + graph.getNodesCount() + ', Edges: ' + graph.getLinksCount());
 
 console.time('Layout time');
 console.log('Performing ' + layoutIterations + ' iterations of layout');
@@ -16,3 +25,4 @@ for (var i = 0; i < layoutIterations; ++i) {
 }
 console.timeEnd('Layout time');
 console.log('Done.');
+
