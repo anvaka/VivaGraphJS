86c2b5cff5d779eea6b3f796af9dc5acedf86eb2
anvaka <anvaka@gmail.com>
2015-02-11 17:15:41 -0800

    do away with anonymous functions

--- src/WebGL/webgl.js
+++ src/WebGL/webgl.js
@@ -7,96 +7,99 @@
 module.exports = webgl;
 
 function webgl(gl) {
-    var createShader = function (shaderText, type) {
-            var shader = gl.createShader(type);
-            gl.shaderSource(shader, shaderText);
-            gl.compileShader(shader);
 
-            if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-                var msg = gl.getShaderInfoLog(shader);
-                window.alert(msg);
-                throw msg;
-            }
+  return {
+    createProgram: createProgram,
+    extendArray: extendArray,
+    copyArrayPart: copyArrayPart,
+    swapArrayPart: swapArrayPart,
+    getLocations: getLocations,
+    context: gl
+  };
 
-            return shader;
-        };
+  function createShader(shaderText, type) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, shaderText);
+    gl.compileShader(shader);
 
-    return {
-        createProgram : function (vertexShaderSrc, fragmentShaderSrc) {
-            var program = gl.createProgram(),
-                vs = createShader(vertexShaderSrc, gl.VERTEX_SHADER),
-                fs = createShader(fragmentShaderSrc, gl.FRAGMENT_SHADER);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      var msg = gl.getShaderInfoLog(shader);
+      window.alert(msg);
+      throw msg;
+    }
 
-            gl.attachShader(program, vs);
-            gl.attachShader(program, fs);
-            gl.linkProgram(program);
+    return shader;
+  }
 
-            if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-                var msg = gl.getShaderInfoLog(program);
-                window.alert(msg);
-                throw msg;
-            }
+  function createProgram(vertexShaderSrc, fragmentShaderSrc) {
+    var program = gl.createProgram();
+    var vs = createShader(vertexShaderSrc, gl.VERTEX_SHADER);
+    var fs = createShader(fragmentShaderSrc, gl.FRAGMENT_SHADER);
 
-            return program;
-        },
+    gl.attachShader(program, vs);
+    gl.attachShader(program, fs);
+    gl.linkProgram(program);
 
-        extendArray : function (buffer, itemsInBuffer, elementsPerItem) {
-            if ((itemsInBuffer  + 1) * elementsPerItem > buffer.length) {
-                // Every time we run out of space create new array twice bigger.
-                // TODO: it seems buffer size is limited. Consider using multiple arrays for huge graphs
-                var extendedArray = new Float32Array(buffer.length * elementsPerItem * 2);
-                extendedArray.set(buffer);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+      var msg = gl.getShaderInfoLog(program);
+      window.alert(msg);
+      throw msg;
+    }
 
-                return extendedArray;
-            }
+    return program;
+  }
 
-            return buffer;
-        },
+  function extendArray(buffer, itemsInBuffer, elementsPerItem) {
+    if ((itemsInBuffer + 1) * elementsPerItem > buffer.length) {
+      // Every time we run out of space create new array twice bigger.
+      // TODO: it seems buffer size is limited. Consider using multiple arrays for huge graphs
+      var extendedArray = new Float32Array(buffer.length * elementsPerItem * 2);
+      extendedArray.set(buffer);
 
-        copyArrayPart : function (array, to, from, elementsCount) {
-            var i;
-            for (i = 0; i < elementsCount; ++i) {
-                array[to + i] = array[from + i];
-            }
-        },
+      return extendedArray;
+    }
 
-        swapArrayPart : function (array, from, to, elementsCount) {
-            var i;
-            for (i = 0; i < elementsCount; ++i) {
-                var tmp = array[from + i];
-                array[from + i] = array[to + i];
-                array[to + i] = tmp;
-            }
-        },
+    return buffer;
+  }
 
-        getLocations : function (program, uniformOrAttributeNames) {
-            var foundLocations = {},
-                i;
-            for (i = 0; i < uniformOrAttributeNames.length; ++i) {
-                var name = uniformOrAttributeNames[i],
-                    location = -1;
-                if (name.indexOf("a_") === 0) {
-                    location = gl.getAttribLocation(program, name);
-                    if (location === -1) {
-                        throw "Program doesn't have required attribute: " + name;
-                    }
+  function getLocations(program, uniformOrAttributeNames) {
+    var foundLocations = {};
+    for (var i = 0; i < uniformOrAttributeNames.length; ++i) {
+      var name = uniformOrAttributeNames[i];
+      var location = -1;
+      if (name[0] === 'a' && name[1] === '_') {
+        location = gl.getAttribLocation(program, name);
+        if (location === -1) {
+          throw new Error("Program doesn't have required attribute: " + name);
+        }
 
-                    foundLocations[name.slice(2)] = location;
-                } else if (name.indexOf("u_") === 0) {
-                    location = gl.getUniformLocation(program, name);
-                    if (location === null) {
-                        throw "Program doesn't have required uniform: " + name;
-                    }
+        foundLocations[name.slice(2)] = location;
+      } else if (name[0] === 'u' && name[1] === '_') {
+        location = gl.getUniformLocation(program, name);
+        if (location === null) {
+          throw new Error("Program doesn't have required uniform: " + name);
+        }
 
-                    foundLocations[name.slice(2)] = location;
-                } else {
-                    throw "Couldn't figure out your intent. All uniforms should start with 'u_' prefix, and attributes with 'a_'";
-                }
-            }
+        foundLocations[name.slice(2)] = location;
+      } else {
+        throw new Error("Couldn't figure out your intent. All uniforms should start with 'u_' prefix, and attributes with 'a_'");
+      }
+    }
 
-            return foundLocations;
-        },
+    return foundLocations;
+  }
+}
 
-        context : gl
-    };
+function copyArrayPart(array, to, from, elementsCount) {
+  for (var i = 0; i < elementsCount; ++i) {
+    array[to + i] = array[from + i];
+  }
+}
+
+function swapArrayPart(array, from, to, elementsCount) {
+  for (var i = 0; i < elementsCount; ++i) {
+    var tmp = array[from + i];
+    array[from + i] = array[to + i];
+    array[to + i] = tmp;
+  }
 }
