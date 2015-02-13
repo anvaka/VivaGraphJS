52b39b351ec60cc4f52d2b58b8f518bd6ec88c55
anvaka <anvaka@gmail.com>
2015-02-12 19:12:09 -0800

    Removed anonymouse functions

--- src/WebGL/webglNodeProgram.js
+++ src/WebGL/webglNodeProgram.js
@@ -13,134 +13,151 @@ module.exports = webglNodeProgram;
  * Defines simple UI for nodes in webgl renderer. Each node is rendered as square. Color and size can be changed.
  */
 function webglNodeProgram() {
-    var ATTRIBUTES_PER_PRIMITIVE = 4, // Primitive is point, x, y, size, color
-        // x, y, z - floats, color = uint.
-        BYTES_PER_NODE = 3 * Float32Array.BYTES_PER_ELEMENT + Uint32Array.BYTES_PER_ELEMENT,
-        nodesFS = [
-            'precision mediump float;',
-            'varying vec4 color;',
+  var ATTRIBUTES_PER_PRIMITIVE = 4; // Primitive is point, x, y, size, color
+  // x, y, z - floats, color = uint.
+  var BYTES_PER_NODE = 3 * Float32Array.BYTES_PER_ELEMENT + Uint32Array.BYTES_PER_ELEMENT;
+  var nodesFS = [
+    'precision mediump float;',
+    'varying vec4 color;',
 
-            'void main(void) {',
-            '   gl_FragColor = color;',
-            '}'
-        ].join('\n'),
-        nodesVS = [
-            'attribute vec3 a_vertexPos;',
-            'attribute vec4 a_color;',
-            'uniform vec2 u_screenSize;',
-            'uniform mat4 u_transform;',
-            'varying vec4 color;',
+    'void main(void) {',
+    '   gl_FragColor = color;',
+    '}'
+  ].join('\n');
+  var nodesVS = [
+    'attribute vec3 a_vertexPos;',
+    'attribute vec4 a_color;',
+    'uniform vec2 u_screenSize;',
+    'uniform mat4 u_transform;',
+    'varying vec4 color;',
 
-            'void main(void) {',
-            '   gl_Position = u_transform * vec4(a_vertexPos.xy/u_screenSize, 0, 1);',
-            '   gl_PointSize = a_vertexPos.z * u_transform[0][0];',
-            '   color = a_color.abgr;',
-            '}'
-        ].join('\n'),
+    'void main(void) {',
+    '   gl_Position = u_transform * vec4(a_vertexPos.xy/u_screenSize, 0, 1);',
+    '   gl_PointSize = a_vertexPos.z * u_transform[0][0];',
+    '   color = a_color.abgr;',
+    '}'
+  ].join('\n');
 
-        program,
-        gl,
-        buffer,
-        locations,
-        utils,
-        storage = new ArrayBuffer(16 * BYTES_PER_NODE),
-        positions = new Float32Array(storage),
-        colors = new Uint32Array(storage),
-        nodesCount = 0,
-        width,
-        height,
-        transform,
-        sizeDirty,
+  var program;
+  var gl;
+  var buffer;
+  var locations;
+  var utils;
+  var storage = new ArrayBuffer(16 * BYTES_PER_NODE);
+  var positions = new Float32Array(storage);
+  var colors = new Uint32Array(storage);
+  var nodesCount = 0;
+  var width;
+  var height;
+  var transform;
+  var sizeDirty;
 
-        ensureEnoughStorage = function () {
-            if ((nodesCount + 1) * BYTES_PER_NODE >= storage.byteLength) {
-                // Every time we run out of space create new array twice bigger.
-                // TODO: it seems buffer size is limited. Consider using multiple arrays for huge graphs
-                var extendedStorage = new ArrayBuffer(storage.byteLength * 2),
-                    extendedPositions = new Float32Array(extendedStorage),
-                    extendedColors = new Uint32Array(extendedStorage);
+  return {
+    load: load,
 
-                extendedColors.set(colors); // should be enough to copy just one view.
-                positions = extendedPositions;
-                colors = extendedColors;
-                storage = extendedStorage;
-            }
-        };
+    /**
+     * Updates position of node in the buffer of nodes.
+     *
+     * @param idx - index of current node.
+     * @param pos - new position of the node.
+     */
+    position: position,
 
-    return {
-        load : function (glContext) {
-            gl = glContext;
-            utils = glUtils(glContext);
+    updateTransform: updateTransform,
 
-            program = utils.createProgram(nodesVS, nodesFS);
-            gl.useProgram(program);
-            locations = utils.getLocations(program, ['a_vertexPos', 'a_color', 'u_screenSize', 'u_transform']);
+    updateSize: updateSize,
 
-            gl.enableVertexAttribArray(locations.vertexPos);
-            gl.enableVertexAttribArray(locations.color);
+    removeNode: removeNode,
 
-            buffer = gl.createBuffer();
-        },
+    createNode: createNode,
 
-        /**
-         * Updates position of node in the buffer of nodes.
-         *
-         * @param idx - index of current node.
-         * @param pos - new position of the node.
-         */
-        position : function (nodeUI, pos) {
-            var idx = nodeUI.id;
+    replaceProperties: replaceProperties,
 
-            positions[idx * ATTRIBUTES_PER_PRIMITIVE] = pos.x;
-            positions[idx * ATTRIBUTES_PER_PRIMITIVE + 1] = pos.y;
-            positions[idx * ATTRIBUTES_PER_PRIMITIVE + 2] = nodeUI.size;
+    render: render
+  };
 
-            colors[idx * ATTRIBUTES_PER_PRIMITIVE + 3] = nodeUI.color;
-        },
+  function ensureEnoughStorage() {
+    if ((nodesCount + 1) * BYTES_PER_NODE >= storage.byteLength) {
+      // Every time we run out of space create new array twice bigger.
+      // TODO: it seems buffer size is limited. Consider using multiple arrays for huge graphs
+      var extendedStorage = new ArrayBuffer(storage.byteLength * 2),
+        extendedPositions = new Float32Array(extendedStorage),
+        extendedColors = new Uint32Array(extendedStorage);
 
-        updateTransform : function (newTransform) {
-            sizeDirty = true;
-            transform = newTransform;
-        },
+      extendedColors.set(colors); // should be enough to copy just one view.
+      positions = extendedPositions;
+      colors = extendedColors;
+      storage = extendedStorage;
+    }
+  }
 
-        updateSize : function (w, h) {
-            width = w;
-            height = h;
-            sizeDirty = true;
-        },
+  function load(glContext) {
+    gl = glContext;
+    utils = glUtils(glContext);
 
-        removeNode : function (node) {
-            if (nodesCount > 0) { nodesCount -= 1; }
+    program = utils.createProgram(nodesVS, nodesFS);
+    gl.useProgram(program);
+    locations = utils.getLocations(program, ['a_vertexPos', 'a_color', 'u_screenSize', 'u_transform']);
 
-            if (node.id < nodesCount && nodesCount > 0) {
-                // we can use colors as a 'view' into array array buffer.
-                utils.copyArrayPart(colors, node.id * ATTRIBUTES_PER_PRIMITIVE, nodesCount * ATTRIBUTES_PER_PRIMITIVE, ATTRIBUTES_PER_PRIMITIVE);
-            }
-        },
-/*jshint unused:false */
-        createNode : function (node) {
-            ensureEnoughStorage();
-            nodesCount += 1;
-        },
+    gl.enableVertexAttribArray(locations.vertexPos);
+    gl.enableVertexAttribArray(locations.color);
 
-        replaceProperties : function (replacedNode, newNode) {},
-/*jshint unused:true */
+    buffer = gl.createBuffer();
+  }
 
-        render : function () {
-            gl.useProgram(program);
-            gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-            gl.bufferData(gl.ARRAY_BUFFER, storage, gl.DYNAMIC_DRAW);
+  function position(nodeUI, pos) {
+    var idx = nodeUI.id;
 
-            if (sizeDirty) {
-                sizeDirty = false;
-                gl.uniformMatrix4fv(locations.transform, false, transform);
-                gl.uniform2f(locations.screenSize, width, height);
-            }
+    positions[idx * ATTRIBUTES_PER_PRIMITIVE] = pos.x;
+    positions[idx * ATTRIBUTES_PER_PRIMITIVE + 1] = pos.y;
+    positions[idx * ATTRIBUTES_PER_PRIMITIVE + 2] = nodeUI.size;
 
-            gl.vertexAttribPointer(locations.vertexPos, 3, gl.FLOAT, false, ATTRIBUTES_PER_PRIMITIVE * Float32Array.BYTES_PER_ELEMENT, 0);
-            gl.vertexAttribPointer(locations.color, 4, gl.UNSIGNED_BYTE, true, ATTRIBUTES_PER_PRIMITIVE * Float32Array.BYTES_PER_ELEMENT, 3 * 4);
+    colors[idx * ATTRIBUTES_PER_PRIMITIVE + 3] = nodeUI.color;
+  }
 
-            gl.drawArrays(gl.POINTS, 0, nodesCount);
-        }
-    };
+  function updateTransform(newTransform) {
+    sizeDirty = true;
+    transform = newTransform;
+  }
+
+  function updateSize(w, h) {
+    width = w;
+    height = h;
+    sizeDirty = true;
+  }
+
+  function removeNode(node) {
+      if (nodesCount > 0) {
+        nodesCount -= 1;
+      }
+
+      if (node.id < nodesCount && nodesCount > 0) {
+        // we can use colors as a 'view' into array array buffer.
+        utils.copyArrayPart(colors, node.id * ATTRIBUTES_PER_PRIMITIVE, nodesCount * ATTRIBUTES_PER_PRIMITIVE, ATTRIBUTES_PER_PRIMITIVE);
+      }
+    }
+
+  function createNode() {
+    ensureEnoughStorage();
+    nodesCount += 1;
+  }
+
+  function replaceProperties(/* replacedNode, newNode */) {}
+
+  function render() {
+    gl.useProgram(program);
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, storage, gl.DYNAMIC_DRAW);
+
+    if (sizeDirty) {
+      sizeDirty = false;
+      gl.uniformMatrix4fv(locations.transform, false, transform);
+      gl.uniform2f(locations.screenSize, width, height);
+    }
+
+    gl.vertexAttribPointer(locations.vertexPos, 3, gl.FLOAT, false, ATTRIBUTES_PER_PRIMITIVE * Float32Array.BYTES_PER_ELEMENT, 0);
+    gl.vertexAttribPointer(locations.color, 4, gl.UNSIGNED_BYTE, true, ATTRIBUTES_PER_PRIMITIVE * Float32Array.BYTES_PER_ELEMENT, 3 * 4);
+
+    gl.drawArrays(gl.POINTS, 0, nodesCount);
+  }
 }
