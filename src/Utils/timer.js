d317589d0df5bf0eea997db8542b6cff86b85d53
anvaka <anvaka@gmail.com>
2015-02-14 12:54:44 -0800

    More grooming - less anonymous functions

--- src/Utils/timer.js
+++ src/Utils/timer.js
@@ -5,77 +5,88 @@
 module.exports = createTimer();
 
 function createTimer() {
-    var lastTime = 0,
-        vendors = ['ms', 'moz', 'webkit', 'o'],
-        i,
-        scope;
+  var lastTime = 0,
+    vendors = ['ms', 'moz', 'webkit', 'o'],
+    i,
+    scope;
 
-    if (typeof window !== 'undefined') {
-        scope = window;
-    } else if (typeof global !== 'undefined') {
-        scope = global;
-    } else {
-        scope = {
-            setTimeout: function () {},
-            clearTimeout: function () {}
-        };
+  if (typeof window !== 'undefined') {
+    scope = window;
+  } else if (typeof global !== 'undefined') {
+    scope = global;
+  } else {
+    scope = {
+      setTimeout: noop,
+      clearTimeout: noop
+    };
+  }
+
+  for (i = 0; i < vendors.length && !scope.requestAnimationFrame; ++i) {
+    var vendorPrefix = vendors[i];
+    scope.requestAnimationFrame = scope[vendorPrefix + 'RequestAnimationFrame'];
+    scope.cancelAnimationFrame =
+      scope[vendorPrefix + 'CancelAnimationFrame'] || scope[vendorPrefix + 'CancelRequestAnimationFrame'];
+  }
+
+  if (!scope.requestAnimationFrame) {
+    scope.requestAnimationFrame = rafPolyfill;
+  }
+
+  if (!scope.cancelAnimationFrame) {
+    scope.cancelAnimationFrame = cancelRafPolyfill;
+  }
+
+  return timer;
+
+  /**
+   * Timer that fires callback with given interval (in ms) until
+   * callback returns true;
+   */
+  function timer(callback) {
+    var intervalId;
+    startTimer(); // start it right away.
+
+    return {
+      /**
+       * Stops execution of the callback
+       */
+      stop: stopTimer,
+
+      restart: restart
+    };
+
+    function startTimer() {
+      intervalId = scope.requestAnimationFrame(startTimer);
+      if (!callback()) {
+        stopTimer();
+      }
     }
-    for (i = 0; i < vendors.length && !scope.requestAnimationFrame; ++i) {
-        var vendorPrefix = vendors[i];
-        scope.requestAnimationFrame = scope[vendorPrefix + 'RequestAnimationFrame'];
-        scope.cancelAnimationFrame =
-            scope[vendorPrefix + 'CancelAnimationFrame'] || scope[vendorPrefix + 'CancelRequestAnimationFrame'];
+
+    function stopTimer() {
+      scope.cancelAnimationFrame(intervalId);
+      intervalId = 0;
     }
 
-    if (!scope.requestAnimationFrame) {
-        scope.requestAnimationFrame = function (callback) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = scope.setTimeout(function () { callback(currTime + timeToCall); }, timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
+    function restart() {
+      if (!intervalId) {
+        startTimer();
+      }
     }
+  }
 
-    if (!scope.cancelAnimationFrame) {
-        scope.cancelAnimationFrame = function (id) {
-            scope.clearTimeout(id);
-        };
-    }
+  function rafPolyfill(callback) {
+    var currTime = new Date().getTime();
+    var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+    var id = scope.setTimeout(function() {
+      callback(currTime + timeToCall);
+    }, timeToCall);
+    lastTime = currTime + timeToCall;
+    return id;
+  }
 
-    return timer;
-
-    /**
-     * Timer that fires callback with given interval (in ms) until
-     * callback returns true;
-     */
-    function timer(callback) {
-        var intervalId,
-            stopTimer = function () {
-                scope.cancelAnimationFrame(intervalId);
-                intervalId = 0;
-            },
-
-            startTimer = function () {
-                intervalId = scope.requestAnimationFrame(startTimer);
-                if (!callback()) {
-                    stopTimer();
-                }
-            };
-
-        startTimer(); // start it right away.
-
-        return {
-            /**
-             * Stops execution of the callback
-             */
-            stop: stopTimer,
-
-            restart : function () {
-                if (!intervalId) {
-                    startTimer();
-                }
-            }
-        };
-    }
+  function cancelRafPolyfill(id) {
+    scope.clearTimeout(id);
+  }
 }
+
+function noop() {}
