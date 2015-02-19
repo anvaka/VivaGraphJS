e1b2f0a394f601f8914d477615597d090063dc1a
anvaka <anvaka@gmail.com>
2015-02-18 20:59:02 -0800

    Refactored atlas, removed anonymous functions

--- src/WebGL/webglAtlas.js
+++ src/WebGL/webglAtlas.js
@@ -11,171 +11,192 @@ module.exports = webglAtlas;
  *          parameter a new canvas will be created.
  */
 function webglAtlas(tilesPerTexture) {
-    var tilesPerRow = Math.sqrt(tilesPerTexture || 1024) << 0,
-        tileSize = tilesPerRow,
-        lastLoadedIdx = 1,
-        loadedImages = {},
-        dirtyTimeoutId,
-        skipedDirty = 0,
-        textures = [],
-        trackedUrls = [],
-        that,
+  var tilesPerRow = Math.sqrt(tilesPerTexture || 1024) << 0,
+    tileSize = tilesPerRow,
+    lastLoadedIdx = 1,
+    loadedImages = {},
+    dirtyTimeoutId,
+    skipedDirty = 0,
+    textures = [],
+    trackedUrls = [];
 
-        isPowerOf2 = function (n) {
-            return (n & (n - 1)) === 0;
-        },
-        createTexture = function () {
-            var texture = new Texture(tilesPerRow * tileSize);
-            textures.push(texture);
-        },
-        getTileCoordinates = function (absolutePosition) {
-            var textureNumber = (absolutePosition / tilesPerTexture) << 0,
-                localTileNumber =  (absolutePosition % tilesPerTexture),
-                row = (localTileNumber / tilesPerRow) << 0,
-                col = (localTileNumber % tilesPerRow);
+  if (!isPowerOf2(tilesPerTexture)) {
+    throw "Tiles per texture should be power of two.";
+  }
 
-            return {textureNumber : textureNumber, row : row, col: col};
-        },
-        markDirtyNow = function () {
-            that.isDirty = true;
-            skipedDirty = 0;
-            dirtyTimeoutId = null;
-        },
-        markDirty = function () {
-            // delay this call, since it results in texture reload
-            if (dirtyTimeoutId) {
-                window.clearTimeout(dirtyTimeoutId);
-                skipedDirty += 1;
-                dirtyTimeoutId = null;
-            }
+  // this is the return object
+  var api = {
+    /**
+     * indicates whether atlas has changed texture in it. If true then
+     * some of the textures has isDirty flag set as well.
+     */
+    isDirty: false,
 
-            if (skipedDirty > 10) {
-                markDirtyNow();
-            } else {
-                dirtyTimeoutId = window.setTimeout(markDirtyNow, 400);
-            }
-        },
+    /**
+     * Clears any signs of atlas changes.
+     */
+    clearDirty: clearDirty,
 
-        copy = function (from, to) {
-            var fromCanvas = textures[from.textureNumber].canvas,
-                toCtx = textures[to.textureNumber].ctx,
-                x = to.col * tileSize,
-                y = to.row * tileSize;
+    /**
+     * Removes given url from collection of tiles in the atlas.
+     */
+    remove: remove,
 
-            toCtx.drawImage(fromCanvas, from.col * tileSize, from.row * tileSize, tileSize, tileSize, x, y, tileSize, tileSize);
-            textures[from.textureNumber].isDirty = true;
-            textures[to.textureNumber].isDirty = true;
-        },
+    /**
+     * Gets all textures in the atlas.
+     */
+    getTextures: getTextures,
 
-        drawAt = function (tileNumber, img, callback) {
-            var tilePosition = getTileCoordinates(tileNumber),
-                coordinates = { offset : tileNumber };
+    /**
+     * Gets coordinates of the given image in the atlas. Coordinates is an object:
+     * {offset : int } - where offset is an absolute position of the image in the
+     * atlas.
+     *
+     * Absolute means it can be larger than tilesPerTexture parameter, and in that
+     * case clients should get next texture in getTextures() collection.
+     */
+    getCoordinates: getCoordinates,
 
-            if (tilePosition.textureNumber >= textures.length) {
-                createTexture();
-            }
-            var currentTexture = textures[tilePosition.textureNumber];
+    /**
+     * Asynchronously Loads the image to the atlas. Cross-domain security
+     * limitation applies.
+     */
+    load: load
+  };
 
-            currentTexture.ctx.drawImage(img, tilePosition.col * tileSize, tilePosition.row * tileSize, tileSize, tileSize);
-            trackedUrls[tileNumber] = img.src;
+  return api;
 
-            loadedImages[img.src] = coordinates;
-            currentTexture.isDirty = true;
+  function clearDirty() {
+    var i;
+    api.isDirty = false;
+    for (i = 0; i < textures.length; ++i) {
+      textures[i].isDirty = false;
+    }
+  }
 
-            callback(coordinates);
-        };
+  function remove(imgUrl) {
+    var coordinates = loadedImages[imgUrl];
+    if (!coordinates) {
+      return false;
+    }
+    delete loadedImages[imgUrl];
+    lastLoadedIdx -= 1;
 
-    if (!isPowerOf2(tilesPerTexture)) {
-        throw "Tiles per texture should be power of two.";
+
+    if (lastLoadedIdx === coordinates.offset) {
+      return true; // Ignore if it's last image in the whole set.
     }
 
-    // this is the return object
-    that = {
-        /**
-         * indicates whether atlas has changed texture in it. If true then
-         * some of the textures has isDirty flag set as well.
-         */
-        isDirty : false,
+    var tileToRemove = getTileCoordinates(coordinates.offset),
+      lastTileInSet = getTileCoordinates(lastLoadedIdx);
 
-        /**
-         * Clears any signs of atlas changes.
-         */
-        clearDirty : function () {
-            var i;
-            this.isDirty = false;
-            for (i = 0; i < textures.length; ++i) {
-                textures[i].isDirty = false;
-            }
-        },
+    copy(lastTileInSet, tileToRemove);
 
-        /**
-         * Removes given url from collection of tiles in the atlas.
-         */
-        remove : function (imgUrl) {
-            var coordinates = loadedImages[imgUrl];
-            if (!coordinates) { return false; }
-            delete loadedImages[imgUrl];
-            lastLoadedIdx -= 1;
+    var replacedOffset = loadedImages[trackedUrls[lastLoadedIdx]];
+    replacedOffset.offset = coordinates.offset;
+    trackedUrls[coordinates.offset] = trackedUrls[lastLoadedIdx];
 
+    markDirty();
+    return true;
+  }
 
-            if (lastLoadedIdx === coordinates.offset) {
-                return true; // Ignore if it's last image in the whole set.
-            }
+  function getTextures() {
+    return textures; // I trust you...
+  }
 
-            var tileToRemove = getTileCoordinates(coordinates.offset),
-                lastTileInSet = getTileCoordinates(lastLoadedIdx);
+  function getCoordinates(imgUrl) {
+    return loadedImages[imgUrl];
+  }
 
-            copy(lastTileInSet, tileToRemove);
+  function load(imgUrl, callback) {
+    if (loadedImages.hasOwnProperty(imgUrl)) {
+      callback(loadedImages[imgUrl]);
+    } else {
+      var img = new window.Image(),
+        imgId = lastLoadedIdx;
 
-            var replacedOffset = loadedImages[trackedUrls[lastLoadedIdx]];
-            replacedOffset.offset = coordinates.offset;
-            trackedUrls[coordinates.offset] = trackedUrls[lastLoadedIdx];
+      lastLoadedIdx += 1;
+      img.crossOrigin = "anonymous";
+      img.onload = function() {
+        markDirty();
+        drawAt(imgId, img, callback);
+      };
 
-            markDirty();
-            return true;
-        },
+      img.src = imgUrl;
+    }
+  }
 
-        /**
-         * Gets all textures in the atlas.
-         */
-        getTextures : function () {
-            return textures; // I trust you...
-        },
+  function createTexture() {
+    var texture = new Texture(tilesPerRow * tileSize);
+    textures.push(texture);
+  }
 
-        /**
-         * Gets coordinates of the given image in the atlas. Coordinates is an object:
-         * {offset : int } - where offset is an absolute position of the image in the
-         * atlas.
-         *
-         * Absolute means it can be larger than tilesPerTexture parameter, and in that
-         * case clients should get next texture in getTextures() collection.
-         */
-        getCoordinates : function (imgUrl) {
-            return loadedImages[imgUrl];
-        },
+  function drawAt(tileNumber, img, callback) {
+    var tilePosition = getTileCoordinates(tileNumber),
+      coordinates = {
+        offset: tileNumber
+      };
 
-        /**
-         * Asynchronously Loads the image to the atlas. Cross-domain security
-         * limitation applies.
-         */
-        load : function (imgUrl, callback) {
-            if (loadedImages.hasOwnProperty(imgUrl)) {
-                callback(loadedImages[imgUrl]);
-            } else {
-                var img = new window.Image(),
-                    imgId = lastLoadedIdx;
+    if (tilePosition.textureNumber >= textures.length) {
+      createTexture();
+    }
+    var currentTexture = textures[tilePosition.textureNumber];
 
-                lastLoadedIdx += 1;
-                img.crossOrigin = "anonymous";
-                img.onload = function () {
-                    markDirty();
-                    drawAt(imgId, img, callback);
-                };
+    currentTexture.ctx.drawImage(img, tilePosition.col * tileSize, tilePosition.row * tileSize, tileSize, tileSize);
+    trackedUrls[tileNumber] = img.src;
 
-                img.src = imgUrl;
-            }
-        }
+    loadedImages[img.src] = coordinates;
+    currentTexture.isDirty = true;
+
+    callback(coordinates);
+  }
+
+  function getTileCoordinates(absolutePosition) {
+    var textureNumber = (absolutePosition / tilesPerTexture) << 0,
+      localTileNumber = (absolutePosition % tilesPerTexture),
+      row = (localTileNumber / tilesPerRow) << 0,
+      col = (localTileNumber % tilesPerRow);
+
+    return {
+      textureNumber: textureNumber,
+      row: row,
+      col: col
     };
+  }
 
-    return that;
+  function markDirtyNow() {
+    api.isDirty = true;
+    skipedDirty = 0;
+    dirtyTimeoutId = null;
+  }
+
+  function markDirty() {
+    // delay this call, since it results in texture reload
+    if (dirtyTimeoutId) {
+      window.clearTimeout(dirtyTimeoutId);
+      skipedDirty += 1;
+      dirtyTimeoutId = null;
+    }
+
+    if (skipedDirty > 10) {
+      markDirtyNow();
+    } else {
+      dirtyTimeoutId = window.setTimeout(markDirtyNow, 400);
+    }
+  }
+
+  function copy(from, to) {
+    var fromCanvas = textures[from.textureNumber].canvas,
+      toCtx = textures[to.textureNumber].ctx,
+      x = to.col * tileSize,
+      y = to.row * tileSize;
+
+    toCtx.drawImage(fromCanvas, from.col * tileSize, from.row * tileSize, tileSize, tileSize, x, y, tileSize, tileSize);
+    textures[from.textureNumber].isDirty = true;
+    textures[to.textureNumber].isDirty = true;
+  }
+}
+
+function isPowerOf2(n) {
+  return (n & (n - 1)) === 0;
 }
