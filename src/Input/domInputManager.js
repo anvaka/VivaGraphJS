979b533a13cc8f723e135a43debc956323bb593e
anvaka <anvaka@gmail.com>
2015-03-03 19:11:32 -0800

    Removed anonymous function

--- src/Input/domInputManager.js
+++ src/Input/domInputManager.js
@@ -7,39 +7,41 @@ module.exports = domInputManager;
 var dragndrop = require('./dragndrop.js');
 
 function domInputManager(graph, graphics) {
-    var nodeEvents = {};
-    return {
-        /**
-         * Called by renderer to listen to drag-n-drop events from node. E.g. for SVG
-         * graphics we may listen to DOM events, whereas for WebGL the graphics
-         * should provide custom eventing mechanism.
-         *
-         * @param node - to be monitored.
-         * @param handlers - object with set of three callbacks:
-         *   onStart: function(),
-         *   onDrag: function(e, offset),
-         *   onStop: function()
-         */
-        bindDragNDrop : function (node, handlers) {
-            var events;
-            if (handlers) {
-                var nodeUI = graphics.getNodeUI(node.id);
-                events = dragndrop(nodeUI);
-                if (typeof handlers.onStart === 'function') {
-                    events.onStart(handlers.onStart);
-                }
-                if (typeof handlers.onDrag === 'function') {
-                    events.onDrag(handlers.onDrag);
-                }
-                if (typeof handlers.onStop === 'function') {
-                    events.onStop(handlers.onStop);
-                }
+  var nodeEvents = {};
+  return {
+    /**
+     * Called by renderer to listen to drag-n-drop events from node. E.g. for SVG
+     * graphics we may listen to DOM events, whereas for WebGL the graphics
+     * should provide custom eventing mechanism.
+     *
+     * @param node - to be monitored.
+     * @param handlers - object with set of three callbacks:
+     *   onStart: function(),
+     *   onDrag: function(e, offset),
+     *   onStop: function()
+     */
+    bindDragNDrop: bindDragNDrop
+  };
 
-                nodeEvents[node.id] = events;
-            } else if (( events = nodeEvents[node.id] )) {
-                events.release();
-                delete nodeEvents[node.id];
-            }
-        }
-    };
+  function bindDragNDrop(node, handlers) {
+    var events;
+    if (handlers) {
+      var nodeUI = graphics.getNodeUI(node.id);
+      events = dragndrop(nodeUI);
+      if (typeof handlers.onStart === 'function') {
+        events.onStart(handlers.onStart);
+      }
+      if (typeof handlers.onDrag === 'function') {
+        events.onDrag(handlers.onDrag);
+      }
+      if (typeof handlers.onStop === 'function') {
+        events.onStop(handlers.onStop);
+      }
+
+      nodeEvents[node.id] = events;
+    } else if ((events = nodeEvents[node.id])) {
+      events.release();
+      delete nodeEvents[node.id];
+    }
+  }
 }
